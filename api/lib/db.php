866cc076ff95f647876dc9e2c7e19bb649b467f7
MFernandaRicarte <maria.fernanda.fernandes.ricarte@ccc.ufcg.edu.br>
2025-11-30 12:19:51 -0300

    ajuste put e delete de bens

---
+++ api/lib/db.php
@@ -0,0 +1,21 @@
+<?php
+
+function db(): PDO
+{
+    static $pdo = null;
+
+    if ($pdo instanceof PDO) {
+        return $pdo;
+    }
+
+    $dsn  = 'mysql:host=127.0.0.1;port=3307;dbname=secti;charset=utf8mb4';
+    $user = 'secti';
+    $pass = 'secti';
+
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+
+    return $pdo;
+}
