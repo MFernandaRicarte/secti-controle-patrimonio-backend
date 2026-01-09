04336b1402baa6fe27cf5eae2fba5ff61462f280
Androthorn <andrefigrox@gmail.com>
2026-01-09 11:09:09 -0300

    add: schema unificado para subir mysql

--- api/config/config.php
+++ api/config/config.php
@@ -1,15 +1,17 @@
 <?php
 // Credenciais do seu MySQL local (Docker)
 const DB_HOST = '127.0.0.1';
-const DB_PORT = 3307;
+const DB_PORT = 3306;
 const DB_NAME = 'secti';
-const DB_USER = 'secti';
-const DB_PASS = 'secti';
+const DB_USER = 'root';
+const DB_PASS = '';
 
-function db(): PDO {
+function db(): PDO
+{
     static $pdo;
-    if ($pdo) return $pdo;
-    $dsn = 'mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME.';charset=utf8mb4';
+    if ($pdo)
+        return $pdo;
+    $dsn = 'mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME . ';charset=utf8mb4';
     $pdo = new PDO($dsn, DB_USER, DB_PASS, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
