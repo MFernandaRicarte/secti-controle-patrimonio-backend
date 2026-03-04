d24795004e37fca931cfa89df26ad9ecb85e5772
MarcienioDev <dinizmarcienio@gmail.com>
2026-03-04 12:38:18 -0300

    fix: new config

--- api/config/config.php
+++ api/config/config.php
@@ -1,9 +1,9 @@
 <?php
-const DB_HOST = '127.0.0.1';
-const DB_PORT = 3307;
-const DB_NAME = 'secti';
-const DB_USER = 'secti_user';
-const DB_PASS = 'secti123';
+const DB_HOST = '186.227.194.2';
+const DB_PORT = 3306;
+const DB_NAME = 'secticampinagran_administrativo';
+const DB_USER = 'secticampinagran_fernanda';
+const DB_PASS = 'sectiadmin2025';
 
 function db(): PDO
 {
